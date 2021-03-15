e20f92fc4795b452099f623bf06cfe0b1c7cb003
nicolas <nicolas110996@example.com>
2021-03-15 19:29:15 -0300

    Add new test

--- application/tests/test_dog.py
+++ application/tests/test_dog.py
@@ -10,3 +10,7 @@ class TestDog:
     def test_it_should_print_bark_two_times(self):
         dog = Dog()
         assert "guauguau" == dog.greet(2)
+
+    def test_it_should_print_bark_3_times(self):
+        dog = Dog()
+        assert "guauguauguau" == dog.greet(3)
